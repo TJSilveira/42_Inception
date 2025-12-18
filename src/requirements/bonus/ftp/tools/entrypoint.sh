66e0ac029bc46be966e7917255f74758258dd59b
Tiago Silveira <tiagojesusilveira@gmail.com>
2025-12-18 13:04:03 +0100

    Finish setup FTP server

--- src/requirements/bonus/ftp/tools/entrypoint.sh
+++ src/requirements/bonus/ftp/tools/entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-service vsftpd start
+if [ ! -f "/etc/vsftpd/.configured" ]; then
+	touch /etc/vsftpd/.configured
+
+	adduser "$FTP_USER" --disabled-password
+	echo "$FTP_USER:$FTP_PASSWORD" | /usr/sbin/chpasswd &> /dev/null
+	chown -R "$FTP_USER:$FTP_USER" /var/www/html
+	echo "$FTP_USER" | tee -a /etc/vsftpd.userlist &> /dev/null
+fi
+
+exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
